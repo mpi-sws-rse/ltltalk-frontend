6f47dd9d61c1972a927303b2f50f6d0b126cd71e
Chuntong Gao <heihegao@gmail.com>
2019-08-15 14:10:09 +0200

    fix click issue in task list

--- voxelurn/src/components/TaskList/index.js
+++ voxelurn/src/components/TaskList/index.js
@@ -8,90 +8,110 @@ class TaskList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-		  displayTask: false
+			displayTask: false
 		};
-		
-		
+
 		this.showTask = this.showTask.bind(this);
 		this.closeTask = this.closeTask.bind(this);
 		this.handleTask = this.handleTask.bind(this);
-
-	
 	}
-		showTask(event) {
-			event.preventDefault();
-			
-			this.setState({ displayTask: true }, () => {
-				document.addEventListener('click', this.closeTask);
-			});
-		}
-		
-		closeTask(event) {
-			
-				
-				this.setState({ displayTask: false }, () => {
-					document.removeEventListener('click', this.closeTask);
-				});  
-		}
+	showTask(event) {
+		event.preventDefault();
 
+		this.setState({ displayTask: true }, () => {
+			document.addEventListener('click', this.closeTask);
+		});
+	}
 
+	closeTask(event) {
+		this.setState({ displayTask: false }, () => {
+			document.removeEventListener('click', this.closeTask);
+		});
+	}
 
-	handleTask(e,index) {
-		console.log("key id issss")
-		console.log(index)
+	handleTask(e, index) {
+		console.log('key id issss');
+		console.log(index);
 		e.preventDefault();
 		this.props.getTask(index);
 	}
 
-
-	  render() {
-
-		if (this.props.isAnimationEnabled == true || this.props.isKeyPressEnabled== true) {
-
+	render() {
+		if (
+			this.props.isAnimationEnabled == true ||
+			this.props.isKeyPressEnabled == true
+		) {
 			return <span />;
-		} 
+		}
 		else {
-			var names = ["Task 1","Task 2","Task 3","Task 4","Task 5", "Task 6", "Task 7", "Task 8", "Task 9", "Task 10"]
+			var names = [
+				'Task 1',
+				'Task 2',
+				'Task 3',
+				'Task 4',
+				'Task 5',
+				'Task 6',
+				'Task 7',
+				'Task 8',
+				'Task 9',
+				'Task 10'
+			];
 			const that = this;
 
-			return (	
-				
+			return (
+				<div
+					className="task-panel-dropdown"
+					style={{ background: '#4F94CD', width: '200px' }}
+				>
+					<div
+						className="task-panel-dropdown-button"
+						onClick={this.showTask}
+					>
+						{' '}
+						Tasks{' '}
+					</div>
 
-
-		 <div  className="task-panel-dropdown" style = {{background:"#4F94CD",width:"200px"}} >
-         <div className="task-panel-dropdown-button" onClick={this.showTask}> Tasks </div>
-
-          { this.state.displayTask ? (
-          <ul>
-
-				{names.map(function(name, index){
-                    return <li key={ index }><a className="active" href='javascript:void(0)' onClick={(e) => that.handleTask(e,index)} >{name}</a></li>;
-                  })}
-      
-          </ul>
-        ):
-        (
-          null
-        )
-        }
-
-	   </div>
-	   );
-	  }
-	} 
-
+					{this.state.displayTask ? (
+						<ul>
+							{names.map(function(name, index) {
+								return (
+									<li key={index}>
+										<a
+											className="active"
+											style={{
+												width: '100%',
+												display:
+													'inline-block',
+												padding: '8px 16px'
+											}}
+											href="javascript:void(0)"
+											onClick={e =>
+												that.handleTask(
+													e,
+													index
+												)}
+										>
+											{name}
+										</a>
+									</li>
+								);
+							})}
+						</ul>
+					) : null}
+				</div>
+			);
+		}
+	}
 }
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
 	isAnimationEnabled: state.world.isAnimationEnabled,
 	isKeyPressEnabled: state.world.isKeyPressEnabled
 });
 
-
-
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
 	return {
-		getTask: (taskId) => dispatch(Actions.getTask(taskId))
+		getTask: taskId => dispatch(Actions.getTask(taskId))
 	};
 };
 
-export default connect(mapStateToProps,mapDispatchToProps)(TaskList);
+export default connect(mapStateToProps, mapDispatchToProps)(TaskList);
